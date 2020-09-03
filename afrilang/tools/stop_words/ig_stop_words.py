8b2723acdf2ee4e15cf8ce31faaa31f794661cbd
chiazor <kwentohchiazor@gmail.com>
2020-09-03 14:14:13 +0100

    igbo stopwords

--- afrilang/tools/stop_words/ig_stop_words.py
+++ afrilang/tools/stop_words/ig_stop_words.py
@@ -1,3 +1,5 @@
-_stopwords = frozenset(["mụ", "a", "i", "o", "ọ", "m", "nke", "na", "ndị", "bụ", "nari",
+_stopwords = frozenset(["mụ", "a", "i", "o", "ọ", "e", "m", "nke", "na", "ndị", "bụ", "nari",
                         "ya", "ụnụ", "n'", "ka", "kwa", "asatọ", "otu", "abụọ", "iri",
-                        "atọ", "anọ", "ise", "si", "gwa", "dịka", "ihi", "puku", "tupu"])
+                        "atọ", "anọ", "ise", "isii", "asaa", "iteghete", "si", "gwa",
+                        "dịka", "ihi", "puku", "tupu", "niine", "nile", "ugbua", "ụfọdu",
+                        "nari", "olee"])
